c27f13736b39eabd2122dfe2cf1b74cab5d4c075
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-02-10 22:18:25 +0300

    delete "Dont work" text"

--- app/login/video-catalog.login.controller.js
+++ app/login/video-catalog.login.controller.js
@@ -8,9 +8,6 @@
     function Login($scope, $state, loginService) {
 
         $scope.log = log;
-        $scope.validatePassword = function(){
-
-        };
 
         function log() {
             if (loginService.setLogined($scope.loginval, $scope.loginPass)) {
